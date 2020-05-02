a99fda618279c1be9671e91e971b4dc1374ae4ed
FERK <FERK@FERK-PC>
2020-05-02 15:14:31 +0200

    prueba 2

---
+++ IT-Bank-proyecto/src/Prueba/PruebaFer.java
@@ -0,0 +1,14 @@
+package Prueba;
+
+public class PruebaFer {
+
+	public PruebaFer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
